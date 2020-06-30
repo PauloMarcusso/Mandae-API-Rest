0ddee495943c1dcef00a9465d49f7abfdff3d6b9
Paulo H <phmarcusso1@hotmail.com>
2020-06-29 23:49:16 -0300

    conectando com o database

---
+++ src/main/java/com/api/mandae/MandaeApplication.java
@@ -0,0 +1,13 @@
+package com.api.mandae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/mandae/MandaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mandae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
