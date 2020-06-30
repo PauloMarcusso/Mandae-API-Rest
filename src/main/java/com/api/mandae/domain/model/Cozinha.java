162364474894bd3e91501fa9ee7a534d5f3be0bf
Paulo H <phmarcusso1@hotmail.com>
2020-06-30 00:00:38 -0300

    adicionando autoincrement e removendo anotacoes nas colunas

--- src/main/java/com/api/mandae/domain/model/Cozinha.java
+++ src/main/java/com/api/mandae/domain/model/Cozinha.java
@@ -1,18 +1,17 @@
 package com.api.mandae.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "nom_cozinha")
 	private String nome;
 
 	public Long getId() {
--- src/main/java/com/api/mandae/domain/model/Restaurante.java
+++ src/main/java/com/api/mandae/domain/model/Restaurante.java
@@ -4,12 +4,15 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Restaurante {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
