5b81031826da0bdc91ba2bcd113ee4af63ddda95
Paulo H <phmarcusso1@hotmail.com>
2020-06-30 08:08:27 -0300

    excluindo dados da tabela

--- src/main/java/com/api/mandae/jpa/CadastroCozinha.java
+++ src/main/java/com/api/mandae/jpa/CadastroCozinha.java
@@ -29,7 +29,11 @@ public class CadastroCozinha {
 		return manager.find(Cozinha.class, id);
 	}
 	
-	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
 	
 	
 	
---
+++ src/main/java/com/api/mandae/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,24 @@
+package com.api.mandae.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.api.mandae.MandaeApplication;
+import com.api.mandae.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext = new SpringApplicationBuilder(MandaeApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		cadastroCozinha.remover(cozinha);
+	}
+}
