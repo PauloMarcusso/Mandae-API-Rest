3d29bff4ef4039b66b45335056106a6565ec58c0
Paulo H <phmarcusso1@hotmail.com>
2020-07-07 00:47:46 -0300

    criando primeiro controller CozinhaController

---
+++ src/main/java/com/api/mandae/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.api.mandae.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.mandae.domain.model.Cozinha;
+import com.api.mandae.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar(){
+		return cozinhaRepository.listar();
+	}
+}
