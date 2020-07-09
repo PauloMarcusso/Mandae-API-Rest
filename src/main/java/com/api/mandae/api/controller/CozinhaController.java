009933f14a0f7f44d31f6133d17c1e967032c403
Paulo H <phmarcusso1@hotmail.com>
2020-07-09 07:33:03 -0300

    adicionando post ao CozinhaController

--- src/main/java/com/api/mandae/api/controller/CozinhaController.java
+++ src/main/java/com/api/mandae/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.api.mandae.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.mandae.domain.model.Cozinha;
@@ -33,5 +37,11 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void adicionar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
 	}
 }
