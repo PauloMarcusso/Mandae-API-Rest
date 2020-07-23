6cdbb5187015e75b5076d3bc71a43b111835a5f0
Paulo H <phmarcusso1@hotmail.com>
2020-07-23 14:53:03 -0300

    consulta dinamica utilizando jpql

--- src/main/java/com/api/mandae/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/api/mandae/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.api.mandae.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.api.mandae.domain.model.Restaurante;
 import com.api.mandae.domain.repository.RestauranteRepositoryQueries;
@@ -20,13 +23,32 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		
-		var jpql = "from Restaurante where nome like :nome and taxaFrete between : taxaInicial and :taxaFinal";
+		var jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0 = 0 ");
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome","%" + nome + "%")
-				.setParameter("taxaInicial", taxaFreteInicial)
-				.setParameter("taxaFinal", taxaFreteFinal)
-				.getResultList();
+		var parametros = new HashMap<String, Object>();
+		
+		if(StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%" + nome + "%");
+		}
+		
+		
+		if(taxaFreteInicial != null) {
+			jpql.append("and taxaFrete >= :taxaInicial ");
+			parametros.put("taxaInicial", taxaFreteInicial);
+		}
+		
+		if(taxaFreteInicial != null) {
+			jpql.append("and taxaFrete <= :taxaFinal ");
+			parametros.put("taxaFinal", taxaFreteFinal);
+		}
+		
+		TypedQuery<Restaurante> query =  manager.createQuery(jpql.toString(), Restaurante.class);
+		
+		parametros.forEach((chave,valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
 	}
 
 }
