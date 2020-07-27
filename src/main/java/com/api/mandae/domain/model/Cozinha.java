e18f9be454d71cc125f1433dd0229c5d7f31d4e8
Paulo H <phmarcusso1@hotmail.com>
2020-07-27 16:36:20 -0300

    relacionamento @ManyToMany entre Restaurante e FormaPagamento

--- src/main/java/com/api/mandae/domain/model/Cozinha.java
+++ src/main/java/com/api/mandae/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package com.api.mandae.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,4 +27,8 @@ public class Cozinha {
 
 	@Column(nullable = false)
 	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,9 @@ insert into restaurante (nome, taxa_frete, cozinha_id ) values ('Restaurante do 
 insert into restaurante (nome, taxa_frete, cozinha_id ) values ('Restaurante 0800', 4.90, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id ) values ('HEEELOO ITS ME', 2.90, 2);
 
-insert into forma_pagamento(descricao) values ('debito');
-insert into forma_pagamento(descricao) values ('credito');
-insert into forma_pagamento(descricao) values ('dinheiro');
+insert into forma_pagamento(id, descricao) values (1, 'cartão de débito');
+insert into forma_pagamento(id, descricao) values (2, 'cartão de credito');
+insert into forma_pagamento(id, descricao) values (3, 'dinheiro');
 
 insert into permissao(nome, descricao) values ('administrador', 'pode tudo');
 insert into permissao(nome, descricao) values ('usuario', 'pode nada');
@@ -17,3 +17,5 @@ insert into estado(id, nome) values (2, 'Pernambuco');
 
 insert into cidade(nome, estado_id) values('Goianorte', 1);
 insert into cidade(nome, estado_id) values('Recife', 2);
+
+insert into restaurante_forma_pagamento(restaurante_id, forma_pagamento_id) values (1,1), (1,2), (1,3), (2,3), (3,2), (3,3);
