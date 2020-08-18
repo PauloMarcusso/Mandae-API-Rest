17baefe5096fa01eafc7eec8c5a2e39c455f6b15
Paulo H. <phmarcusso1@hotmail.com>
2020-08-18 17:58:39 -0300

    Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/api/mandae/api/controller/RestauranteController.java
+++ src/main/java/com/api/mandae/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +28,7 @@ import com.api.mandae.domain.exception.NegocioException;
 import com.api.mandae.domain.model.Restaurante;
 import com.api.mandae.domain.repository.RestauranteRepository;
 import com.api.mandae.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -50,7 +56,7 @@ public class RestauranteController {
 	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
 		try {
 			return cadastroRestaurante.salvar(restaurante);
-		}catch(CozinhaNaoEncontradaException e) {
+		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
 	}
@@ -60,41 +66,54 @@ public class RestauranteController {
 
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(id);
 
-		BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-		
+		BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro",
+				"produtos");
+
 		try {
 			return cadastroRestaurante.salvar(restauranteAtual);
-		}catch(CozinhaNaoEncontradaException e) {
+		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
 	}
 
 	@PatchMapping("/{id}")
-	public Restaurante atualizaParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizaParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos,
+			HttpServletRequest request) {
 
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(id);
 
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 
 		return atualizar(id, restauranteAtual);
 	}
 
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		// pega o tipo da variável e inputa no novo objeto criado
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
 
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
-			// pegando os atributos do Restaurante
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+		try {
+			// pega o tipo da variável e inputa no novo objeto criado
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-			// acessa um atributo privado
-			field.setAccessible(true);
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
 
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+				// pegando os atributos do Restaurante
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+
+				// acessa um atributo privado
+				field.setAccessible(true);
+
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
 	}
 }
