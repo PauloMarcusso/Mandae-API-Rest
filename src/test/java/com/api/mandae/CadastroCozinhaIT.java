473fbb5c997d68437f9cfe384fcc4bf46abc33a8
Paulo H. <phmarcusso1@hotmail.com>
2020-09-21 15:04:13 -0300

    fazendo migration com flyway antes de cada test

--- src/test/java/com/api/mandae/CadastroCozinhaIT.java
+++ src/test/java/com/api/mandae/CadastroCozinhaIT.java
@@ -2,12 +2,15 @@ package com.api.mandae;
 
 import static io.restassured.RestAssured.given;
 
-import org.hamcrest.Matcher;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -17,16 +20,26 @@ public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private Flyway flyway;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+//		
+		flyway.migrate();
+	}
 
 	@Test
 	public void deveRetornarStatus200QuandoConsultarCozinha() {
-		
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
+	
 		given()
-			.basePath("/cozinhas")
-			.port(port)
-			.accept(ContentType.JSON)
+		.accept(ContentType.JSON)
+//			.basePath("/cozinhas")
+//			.port(port)
 		.when()
 			.get()
 			.then()
@@ -36,16 +49,25 @@ public class CadastroCozinhaIT {
 	@Test
 	public void deveConter4CozinhasQuandoConsultarCozinhas() {
 		
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
 		given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
 			.body("", Matchers.hasSize(4))
 			.body("nome", Matchers.hasItems("Tailandesa", "Indiana"));
+	}
+	
+	@Test
+	public void deveRetornarStatus201QuandoCadastrasCozinha() {
+		
+		given()
+			.body("{ \"nome\": \"Chinesa\"}")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 }
