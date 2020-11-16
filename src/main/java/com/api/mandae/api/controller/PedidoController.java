972673cb6af5a526f85a41295c28ce7d3c18d1ec
Paulo H <phmarcusso1@hotmail.com>
2020-11-16 08:05:27 -0300

    Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/api/mandae/api/controller/PedidoController.java
+++ src/main/java/com/api/mandae/api/controller/PedidoController.java
@@ -11,8 +11,12 @@ import com.api.mandae.domain.model.Pedido;
 import com.api.mandae.domain.model.Usuario;
 import com.api.mandae.domain.repository.PedidoRepository;
 import com.api.mandae.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -35,9 +39,30 @@ public class PedidoController {
     private EmissaoPedidoService emissaoPedido;
 
     @GetMapping
-    public List<PedidoResumoDTO> listar() {
-        return pedidoResumoConverter.toCollectionDTO(pedidoRepository.findAll());
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+
+        List<Pedido> pedidos = pedidoRepository.findAll();
+
+        List<PedidoResumoDTO> pedidosModel = pedidoResumoConverter.toCollectionDTO(pedidos);
+
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if(StringUtils.isNotBlank(campos)){
+            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
     }
+
+//    @GetMapping
+//    public List<PedidoResumoDTO> listar() {
+//        return pedidoResumoConverter.toCollectionDTO(pedidoRepository.findAll());
+//    }
 
     @GetMapping("/{codigoPedido}")
     public PedidoDTO buscar(@PathVariable String codigoPedido) {
@@ -49,16 +74,16 @@ public class PedidoController {
     @ResponseStatus(HttpStatus.CREATED)
     public PedidoDTO adicionar(@RequestBody @Valid PedidoInput pedidoInput) {
 
-        try{
-        Pedido novoPedido = pedidoConverter.toDomainObject(pedidoInput);
+        try {
+            Pedido novoPedido = pedidoConverter.toDomainObject(pedidoInput);
 
-        novoPedido.setCliente(new Usuario());
-        novoPedido.getCliente().setId(1L);
-        novoPedido = emissaoPedido.emitir(novoPedido);
+            novoPedido.setCliente(new Usuario());
+            novoPedido.getCliente().setId(1L);
+            novoPedido = emissaoPedido.emitir(novoPedido);
 
-        return pedidoConverter.toDTO(novoPedido);
+            return pedidoConverter.toDTO(novoPedido);
 
-        }catch (EntidadeNaoEncontradaException e){
+        } catch (EntidadeNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
         }
 
--- src/main/java/com/api/mandae/api/model/PedidoResumoDTO.java
+++ src/main/java/com/api/mandae/api/model/PedidoResumoDTO.java
@@ -1,5 +1,6 @@
 package com.api.mandae.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonFilter("pedidoFilter")
 @Getter
 @Setter
 public class PedidoResumoDTO {
