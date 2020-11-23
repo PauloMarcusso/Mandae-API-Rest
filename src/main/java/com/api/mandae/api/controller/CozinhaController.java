d375ff6910789b96a79695801098a9e92af15229
Paulo H <phmarcusso1@hotmail.com>
2020-11-23 09:10:11 -0300

    Implementando paginação e ordenação em recursos de coleção da API

--- src/main/java/com/api/mandae/api/controller/CozinhaController.java
+++ src/main/java/com/api/mandae/api/controller/CozinhaController.java
@@ -1,74 +1,74 @@
 package com.api.mandae.api.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.api.mandae.api.assembler.cozinha.CozinhaConverter;
 import com.api.mandae.api.model.CozinhaDTO;
 import com.api.mandae.api.model.input.CozinhaInput;
 import com.api.mandae.domain.model.Cozinha;
 import com.api.mandae.domain.repository.CozinhaRepository;
 import com.api.mandae.domain.service.CadastroCozinhaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
-	@Autowired
-	private CozinhaRepository cozinhaRepository;
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
-	@Autowired
-	private CadastroCozinhaService cadastroCozinha;
-	
-	@Autowired
-	private CozinhaConverter cozinhaConverter;
-	
+    @Autowired
+    private CadastroCozinhaService cadastroCozinha;
 
-	@GetMapping
-	public List<CozinhaDTO> listar() {
-		return cozinhaConverter.toCollectionDTO(cozinhaRepository.findAll());
-	}
+    @Autowired
+    private CozinhaConverter cozinhaConverter;
 
-	@GetMapping("/{id}")
-	public CozinhaDTO buscar(@PathVariable Long id) {
-		return cozinhaConverter.toDTO(cadastroCozinha.buscarOuFalhar(id));
-	}
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public CozinhaDTO adicionar(@RequestBody @Valid CozinhaInput cozinhaInput) {
-		
-		Cozinha cozinha = cozinhaConverter.toDomainObject(cozinhaInput);
-		
-		return cozinhaConverter.toDTO(cadastroCozinha.salvar(cozinha));
-	}
+    @GetMapping
+    public Page<CozinhaDTO> listar(Pageable pageable) {
 
-	@PutMapping("/{id}")
-	public CozinhaDTO atualizar(@PathVariable Long id, @RequestBody @Valid CozinhaInput cozinhaInput) {
-		
-			Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(id);
-			
-			cozinhaConverter.copyToDomainObject(cozinhaInput, cozinhaAtual);
+        Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 
-			return cozinhaConverter.toDTO(cadastroCozinha.salvar(cozinhaAtual));
-	}
+        List<CozinhaDTO> cozinhaDTO = cozinhaConverter.toCollectionDTO(cozinhasPage.getContent());
 
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long id) {
-		cadastroCozinha.excluir(id);
-	}
+        Page<CozinhaDTO> cozinhaDTOPage = new PageImpl<>(cozinhaDTO, pageable, cozinhasPage.getTotalElements());
+
+        return cozinhaDTOPage;
+    }
+
+    @GetMapping("/{id}")
+    public CozinhaDTO buscar(@PathVariable Long id) {
+        return cozinhaConverter.toDTO(cadastroCozinha.buscarOuFalhar(id));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CozinhaDTO adicionar(@RequestBody @Valid CozinhaInput cozinhaInput) {
+
+        Cozinha cozinha = cozinhaConverter.toDomainObject(cozinhaInput);
+
+        return cozinhaConverter.toDTO(cadastroCozinha.salvar(cozinha));
+    }
+
+    @PutMapping("/{id}")
+    public CozinhaDTO atualizar(@PathVariable Long id, @RequestBody @Valid CozinhaInput cozinhaInput) {
+
+        Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(id);
+
+        cozinhaConverter.copyToDomainObject(cozinhaInput, cozinhaAtual);
+
+        return cozinhaConverter.toDTO(cadastroCozinha.salvar(cozinhaAtual));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long id) {
+        cadastroCozinha.excluir(id);
+    }
 }
--- src/main/java/com/api/mandae/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/api/mandae/api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -64,6 +65,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.timestamp(OffsetDateTime.now()).build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
 	}
 
 	@Override
