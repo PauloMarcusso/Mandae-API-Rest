8e0fb091490cfef44805bc27d62b9b3f2224dd9e
Paulo H <phmarcusso1@hotmail.com>
2020-11-23 21:29:48 -0300

    Implementando JsonSerializer para customizar representação de paginação

---
+++ src/main/java/com/api/mandae/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,26 @@
+package com.api.mandae.core.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+    @Override
+    public void serialize(Page<?> page, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+        gen.writeStartObject();
+
+        gen.writeObjectField("content", page.getContent());
+        gen.writeNumberField("size", page.getSize());
+        gen.writeNumberField("totalElements", page.getTotalElements());
+        gen.writeNumberField("totalPages", page.getTotalPages());
+        gen.writeNumberField("number", page.getNumber());
+
+        gen.writeEndObject();
+    }
+}
