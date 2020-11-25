fe851d20e042e64f7795e611d41248ddc3d52eb8
Paulo H <phmarcusso1@hotmail.com>
2020-11-24 23:24:18 -0300

    adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/api/mandae/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/api/mandae/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,13 +2,15 @@ package com.api.mandae.infrastructure.service;
 
 import com.api.mandae.domain.filter.VendaDiariaFilter;
 import com.api.mandae.domain.model.Pedido;
+import com.api.mandae.domain.model.StatusPedido;
 import com.api.mandae.domain.model.dto.VendaDiaria;
 import com.api.mandae.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.time.LocalDate;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -24,7 +26,24 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 
         var builder = manager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiaria.class);
+
         var root = query.from(Pedido.class);
+
+        var predicates = new ArrayList<Predicate>();
+
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThan(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThan(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
 
         var functionDateDataCriacao =
                 builder.function("date", Date.class, root.get("dataCriacao"));
@@ -37,6 +56,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         );
 
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateDataCriacao);
 
         return manager.createQuery(query).getResultList();
