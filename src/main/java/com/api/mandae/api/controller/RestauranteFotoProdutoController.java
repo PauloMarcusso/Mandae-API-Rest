33ac1933a24926148195ca069926bbde6c5fbefa
PauloMarcusso <phmarcusso1@gmail.com>
2020-12-10 22:08:00 -0300

    Implementando upload de arquivo com multipart/form-data

---
+++ src/main/java/com/api/mandae/api/controller/RestauranteFotoProdutoController.java
@@ -0,0 +1,34 @@
+package com.api.mandae.api.controller;
+
+import com.api.mandae.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteFotoProdutoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+            FotoProdutoInput fotoProdutoInput) {
+
+        var nomeArquivo = UUID.randomUUID()
+                              .toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+        var arquivoFoto = Path.of("/Users/paulo.h.marcusso/Documents/Estudos/Java/Spring/imagens", nomeArquivo);
+
+        System.out.println(fotoProdutoInput.getDescricao());
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+        try {
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
