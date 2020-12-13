17169f130a917787989241389d15eee80ceb9912
PauloMarcusso <phmarcusso1@gmail.com>
2020-12-13 19:00:53 -0300

    Validando o content type do arquivo

--- src/main/java/com/api/mandae/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/api/mandae/api/model/input/FotoProdutoInput.java
@@ -1,10 +1,12 @@
 package com.api.mandae.api.model.input;
 
 
+import com.api.mandae.core.validation.FileContentType;
 import com.api.mandae.core.validation.FileSize;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -15,6 +17,7 @@ public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile arquivo;
 
     @NotBlank
