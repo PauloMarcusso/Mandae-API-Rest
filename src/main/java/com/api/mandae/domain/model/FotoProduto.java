720adb531a7be27844320fddd54bfafba024a532
PauloMarcusso <phmarcusso1@gmail.com>
2020-12-13 19:24:12 -0300

    Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/api/mandae/domain/model/FotoProduto.java
@@ -0,0 +1,29 @@
+package com.api.mandae.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+public class FotoProduto {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+
+    private String nomeArquivo;
+    private String descricao;
+    private String contentType;
+    private Long tamanho;
+
+}
