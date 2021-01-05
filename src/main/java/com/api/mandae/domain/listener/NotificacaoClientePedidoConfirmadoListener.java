b27e4b9d37644d270741c5cfd8d8b60a6d83404a
PauloMarcusso <phmarcusso1@gmail.com>
2021-01-05 12:25:31 -0300

    Reagindo a Domain Events em fases específicas da transação

--- src/main/java/com/api/mandae/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/api/mandae/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -6,6 +6,8 @@ import com.api.mandae.domain.service.EnvioEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
@@ -13,7 +15,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
     @Autowired
     private EnvioEmailService envioEmail;
 
-    @EventListener
+    @TransactionalEventListener
     public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 
         Pedido pedido = event.getPedido();
