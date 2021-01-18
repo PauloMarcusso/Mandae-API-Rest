ac7f45e69e9308415b14c5116d201e8bb8c2ab51
PauloMarcusso <phmarcusso1@gmail.com>
2021-01-18 13:47:30 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/api/mandae/api/controller/CidadeController.java
+++ src/main/java/com/api/mandae/api/controller/CidadeController.java
@@ -1,6 +1,7 @@
 package com.api.mandae.api.controller;
 
 import com.api.mandae.api.assembler.cidade.CidadeConverter;
+import com.api.mandae.api.exceptionhandler.Problem;
 import com.api.mandae.api.model.CidadeDTO;
 import com.api.mandae.api.model.input.CidadeInput;
 import com.api.mandae.domain.exception.EstadoNaoEncontradoException;
@@ -8,9 +9,7 @@ import com.api.mandae.domain.exception.NegocioException;
 import com.api.mandae.domain.model.Cidade;
 import com.api.mandae.domain.repository.CidadeRepository;
 import com.api.mandae.domain.service.CadastroCidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -39,6 +38,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @GetMapping("/{id}")
     public CidadeDTO buscar(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long id) {
         return cidadeConverter.toDTO(cadastroCidade.buscarOuFalhar(id));
@@ -60,6 +63,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Atualiza uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cidade atualizada"),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @PutMapping("/{id}")
     public CidadeDTO atualizar(
             @ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long id,
@@ -82,6 +89,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Deleta uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Cidade excluída"),
+            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void excluir(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long id) {
