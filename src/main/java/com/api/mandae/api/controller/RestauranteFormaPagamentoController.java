1959b41e7766fd43d754164a20f5f9793cc6bcb3
PauloMarcusso <phmarcusso1@gmail.com>
2021-01-21 12:19:15 -0300

    Descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- src/main/java/com/api/mandae/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/api/mandae/api/controller/RestauranteFormaPagamentoController.java
@@ -2,17 +2,19 @@ package com.api.mandae.api.controller;
 
 import com.api.mandae.api.assembler.formapagamento.FormaPagamentoConverter;
 import com.api.mandae.api.model.FormaPagamentoDTO;
+import com.api.mandae.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.api.mandae.domain.model.Restaurante;
 import com.api.mandae.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(value = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     @Autowired
     private FormaPagamentoConverter formaPagamentoConverter;
