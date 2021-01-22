67b5605aac98c197993e97a2a58e5588a8ae9697
PauloMarcusso <phmarcusso1@gmail.com>
2021-01-22 18:10:45 -0300

    Descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/api/mandae/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/api/mandae/api/controller/UsuarioGrupoController.java
@@ -2,19 +2,21 @@ package com.api.mandae.api.controller;
 
 import com.api.mandae.api.assembler.grupo.GrupoConverter;
 import com.api.mandae.api.model.GrupoDTO;
+import com.api.mandae.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.api.mandae.domain.model.Usuario;
 import com.api.mandae.domain.repository.GrupoRepository;
 import com.api.mandae.domain.service.CadastroGrupoService;
 import com.api.mandae.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "/usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
