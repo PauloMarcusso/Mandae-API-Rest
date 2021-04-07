9e22e6ce51419d580388c5aa0a3305364c5926ad
PauloMarcusso <phmarcusso1@gmail.com>
2021-04-07 08:42:34 -0300

    Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/com/api/mandae/core/security/ResourceServerConfig.java
+++ src/main/java/com/api/mandae/core/security/ResourceServerConfig.java
@@ -1,10 +1,16 @@
 package com.api.mandae.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +23,16 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
+//                .oauth2ResourceServer().opaqueToken();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+
+        var secretKey = new SecretKeySpec("vASVsydvcyavsdavsydvaysvdyasvdTSVIysavdytav".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
