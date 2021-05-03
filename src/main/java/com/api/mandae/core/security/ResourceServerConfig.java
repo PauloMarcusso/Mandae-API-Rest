0c691715deb9440f7172b88c54b37085d4f94839
PauloMarcusso <phmarcusso1@gmail.com>
2021-05-03 08:10:36 -0300

    Carregando Granted Authorities dos escopos do OAuth2 no Resource Server

--- src/main/java/com/api/mandae/core/security/ResourceServerConfig.java
+++ src/main/java/com/api/mandae/core/security/ResourceServerConfig.java
@@ -5,9 +5,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
@@ -37,9 +40,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                         authorities = Collections.emptyList();
                     }
 
-                    return authorities.stream()
+                    var scopesAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+                    Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter.convert(jwt);
+
+                    grantedAuthorities.addAll(authorities.stream()
                             .map(SimpleGrantedAuthority::new)
-                            .collect(Collectors.toList());
+                            .collect(Collectors.toList()));
+
+                    return grantedAuthorities;
                 });
 
         return jwtAuthenticationConverter;
