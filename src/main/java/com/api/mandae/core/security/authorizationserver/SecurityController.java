98ba6883fa0d6abfcf61c0e3ac1241213bd3f4d0
PauloMarcusso <phmarcusso1@gmail.com>
2021-05-11 20:41:40 -0300

    Customizando a página de OAuth2 Approval

--- src/main/java/com/api/mandae/core/security/authorizationserver/SecurityController.java
+++ src/main/java/com/api/mandae/core/security/authorizationserver/SecurityController.java
@@ -10,4 +10,9 @@ public class SecurityController {
     public String login(){
         return "pages/login";
     }
+
+    @GetMapping("/oauth/confirm_access")
+    public String approval(){
+        return "pages/approval";
+    }
 }
