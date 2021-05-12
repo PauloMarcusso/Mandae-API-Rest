8e2d29e5800c89a3f946841f167bfe6673bdacdd
PauloMarcusso <phmarcusso1@gmail.com>
2021-05-11 22:46:46 -0300

    Externalizando o KeyStore: criando um ProtocolResolver para Base64

--- src/main/java/com/api/mandae/MandaeApplication.java
+++ src/main/java/com/api/mandae/MandaeApplication.java
@@ -2,6 +2,7 @@ package com.api.mandae;
 
 import java.util.TimeZone;
 
+import com.api.mandae.core.io.Base64ProtocolResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,7 +15,12 @@ public class MandaeApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication.run(MandaeApplication.class, args);
+
+		var app = new SpringApplication(MandaeApplication.class);
+		app.addListeners(new Base64ProtocolResolver());
+		app.run(args);
+
+//		SpringApplication.run(MandaeApplication.class, args);
 	}
 
 }
