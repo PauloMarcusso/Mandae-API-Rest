0da96ddb539b7a4bdda893536b28879b46615fb5
PauloMarcusso <phmarcusso1@gmail.com>
2021-05-20 23:43:37 -0300

    Resolvendo problemas com storage de Authorization Codes

--- src/main/java/com/api/mandae/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/api/mandae/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -99,6 +100,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
+                .authorizationCodeServices(new JdbcAuthorizationCodeServices(this.dataSource))
                 .accessTokenConverter(jwtAccessTokenConverter())
                 .tokenEnhancer(enhancerChain)
                 .approvalStore(approvalStore(endpoints.getTokenStore()))
